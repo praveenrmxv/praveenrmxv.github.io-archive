16d25b8b8799d0c20ace702e8623fe4074a0eaaf
praveenrmxv <103093467+praveenrmxv@users.noreply.github.com>
2022-04-13 22:48:40 +0530

    praveen
    these all mail file

--- urls.py
+++ urls.py
@@ -1,13 +1,27 @@
-from django.urls import path
-from . import views
+"""Myblog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+admin.site.site_header = "Praveen Admin"
+admin.site.site_title = "Praveen pannel"
+admin.site.index_title = "Welcome to Praveen"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('contact/', views.contact, name="contact"),
-    path('search/', views.search, name="search"),
-    path('signup', views.handleSignUp, name="handleSignUp"),
-    path('login', views.handleLogin, name="handleSignUp"),
-    path('logout', views.hanldeLogout, name="handleSignUp"),
-
-
+    path('', include('home.urls')),
+    path('Aviblog/', include('Aviblog.urls')),
+    path('home/', include('home.urls')),
+    path('admin/', admin.site.urls),
 ]
